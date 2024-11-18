04a1a0638207b9320d6e70af7f4cf56cea364782
ChrisWeijers <c.r.j.weijers@student.tue.nl>
2024-11-18 14:33:12 +0100

    Plotting map with railroads

--- code.py
+++ code.py
@@ -1,10 +1,8 @@
 import numpy as np
-from matplotlib.lines import Line2D
 import pandas as pd
 from shapely.geometry import Point
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import sqlite3
 
 df = pd.read_csv(
     'data/Railroad_Equipment_Accident_Incident.csv',
@@ -14,8 +12,10 @@ df = pd.read_csv(
 
 df['corrected_year'] = np.where(df['YEAR'] > 24.0, 1900+df['YEAR'], 2000+df['YEAR'])
 
-print(df[['Longitud', 'Latitude']].head())
-print(sum(df[['Latitude']].isna()))
+df_points = df[['Longitud', 'Latitude']].dropna()
+print(sum(df['Latitude'].isna()))
+print(sum(df['Longitud'].isna()))
+
 
 def plot_rail(data):
     map_railroads = gpd.read_file(r'C:/Users/Cweij/Downloads/USA_Railroads-shp.zip')
@@ -23,20 +23,27 @@ def plot_rail(data):
     gdf = gpd.GeoDataFrame(geometry=[Point(x, y) for x, y in zip(data['Longitud'], data['Latitude'])])
     gdf.crs = map_railroads.crs
 
-    f, ax = plt.subplots(1, figsize=(8, 8))
+    return gdf, map_railroads
+
+
+gdf, map_railroads = plot_rail(df_points)
+def plot(gdf, map_railroads):
+    f, ax = plt.subplots(1, figsize=(16, 16))
+
+    f.patch.set_facecolor('black')
+    ax.set_facecolor('black')
 
     map_railroads.plot(linewidth=0.5, edgecolor='white', color='lightgrey', legend=True, ax=ax)
-    gdf.plot(ax=ax, marker='o', markersize=10)
+    gdf.plot(ax=ax, marker='o', markersize=3)
 
     ax.set_title('Railroads', size=15)
     ax.set_xlabel('Longitud')
     ax.set_ylabel('Latitude')
+    ax.set_xlim(xmin=-130, xmax=-60)
+    ax.set_ylim(ymin=20, ymax=55)
 
-    legend = [
-        Line2D([0], [0], markerfacecolor='#FF8F35', marker='o', color='w', label='Weather station'),
-        Line2D([0], [0], markerfacecolor='#5499C7', marker='o', color='w', label='Air quality station')
-    ]
-    ax.legend(handles=legend, loc='upper left')
     return ax
 
-plot_rail(df)
+
+plot(gdf, map_railroads)
+
