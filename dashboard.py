de3cf00961da99c826c02bdc072e1248e0d74348
ChrisWeijers <c.r.j.weijers@student.tue.nl>
2024-11-27 20:49:56 +0100

    Changes in dashboard.py, zoom when clicking on state.

--- dashboard.py
+++ dashboard.py
@@ -16,6 +16,12 @@ fips_codes = pd.read_csv(
     'data/state_fips_master.csv',
     delimiter=',',
 )
+
+states_center = pd.read_csv(
+    'data/states_center.csv',
+    delimiter=',',
+)
+
 fips_codes = fips_codes[['fips', 'state_name']].copy()
 
 df['corrected_year'] = np.where(df['YEAR'] > 24.0, 1900 + df['YEAR'], 2000 + df['YEAR'])
@@ -51,14 +57,24 @@ app.layout = html.Div(
         Input('crash-map', 'clickData'),]
 )
 def figure(click_data):
+    if not click_data:
+        zoom = 3
+        center = {'lat': 39.8282, 'lon': -98.5795}
+    else:
+        zoom = 5
+        center = {
+            'lat': states_center.loc[states_center['Name'] == click_data['points'][0]['location'], 'Latitude'].values[0],
+            'lon': states_center.loc[states_center['Name'] == click_data['points'][0]['location'], 'Longitude'].values[0]
+        }
+
     fig = px.choropleth_mapbox(
         geojson=us_states,
         locations=[state['properties']['name'] for state in us_states['features']],
         featureidkey="properties.name",
         hover_name=[state['properties']['name'] for state in us_states['features']],
         color_discrete_sequence=["white"],
-        center={'lat': 39.8282, 'lon': -98.5795},
-        zoom=3,
+        center=center,
+        zoom=zoom,
         height=700,
         opacity=0.03
     )
@@ -67,6 +83,7 @@ def figure(click_data):
 
     fig.update_layout(
         mapbox_style="carto-darkmatter",
+        paper_bgcolor='darkgrey',
     )
 
     if click_data:
