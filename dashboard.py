10ca536151ef996f84cab25b27cd96f5e48854f7
ChrisWeijers <c.r.j.weijers@student.tue.nl>
2024-11-29 14:24:15 +0100

    dashboard.py updatet

--- dashboard.py
+++ dashboard.py
@@ -28,14 +28,11 @@ df['corrected_year'] = np.where(df['YEAR'] > 24.0, 1900 + df['YEAR'], 2000 + df[
 df = df.dropna(subset=['Latitude', 'Longitud']).drop_duplicates(subset=['Latitude', 'Longitud'])
 df = pd.merge(df, fips_codes, left_on='STATE', right_on='fips').drop('fips', axis=1)
 
-
 with open('data/us-states.geojson', 'r') as geojson_file:
     us_states = json.load(geojson_file)
 
 state_count = df.groupby('state_name').size().reset_index(name='crash_count').sort_values(by='crash_count',
                                                                                           ascending=False)
-
-
 
 app = Dash(__name__, assets_folder='assets')
 
@@ -47,9 +44,9 @@ app.layout = html.Div(
             id='crash-map',
             className='graph-container',
             config={
-                'scrollZoom': True,       # Enable scroll zoom
-                'doubleClick': 'reset',   # Optional: Double-click behavior
-                'displayModeBar': True,   # Optional: Show mode bar for additional controls
+                'scrollZoom': True,  # Enable scroll zoom
+                'doubleClick': 'reset',  # Optional: Double-click behavior
+                'displayModeBar': True,  # Optional: Show mode bar for additional controls
             },
         ),
 
@@ -63,6 +60,7 @@ app.layout = html.Div(
     }
 )
 
+
 @app.callback(
     [Output('crash-map', 'figure'),
      Output('barchart', 'figure')],
@@ -70,9 +68,11 @@ app.layout = html.Div(
      Input('barchart', 'hoverData'),
      Input('barchart', 'clickData'),
      Input('crash-map', 'clickData'),
+     Input('crash-map', 'relayoutData'),
      ]
 )
-def update_map(hover_map, hover_bar, click_bar, click_data):
+def update_map(hover_map, hover_bar, click_bar, click_data, relayout):
+    global state
 
     bar = px.bar(state_count,
                  x='state_name',
@@ -96,18 +96,18 @@ def update_map(hover_map, hover_bar, click_bar, click_data):
     )
 
     fig = px.choropleth_mapbox(
-        state_count,  # Use the state_count DataFrame
+        state_count,
         geojson=us_states,
         locations='state_name',
         featureidkey="properties.name",
-        color='crash_count',  # Map crash_count to color
-        color_continuous_scale="Blues",  # Choose a color scale that represents intensity
-        hover_name='state_name',  # Display state name on hover
-        hover_data={'crash_count': True},  # Include crash_count in hover
+        hover_name='state_name',
+        hover_data={'crash_count': True},
         center={'lat': 39.8282, 'lon': -98.5795},
         zoom=3,
         height=700,
-        opacity=0,  # Adjust opacity
+        opacity=0.1,
+        color_discrete_sequence=['darkgrey'],
+
     )
 
     fig.update_traces(
@@ -115,7 +115,7 @@ def update_map(hover_map, hover_bar, click_bar, click_data):
         marker_line_width=1,
         hovertemplate="<b>%{hovertext}</b><br>Crashes: %{customdata[0]}<extra></extra>",
         hoverlabel=dict(
-            bgcolor="rgba(255, 255, 255, 0.8)",  # Semi-transparent white
+            bgcolor="rgba(255, 255, 255, 0.8)",
             bordercolor="gray",
             font=dict(
                 size=14,
@@ -129,106 +129,56 @@ def update_map(hover_map, hover_bar, click_bar, click_data):
         mapbox_style="carto-darkmatter",
         font=dict(color='white', size=12),
         paper_bgcolor='darkgrey',
-        showlegend=False
+        showlegend=False,
     )
 
-    if hover_map or hover_bar:
-        if hover_bar:
-            hovered_state = hover_bar['points'][0]['label']
+    if hover_map:
+        hovered_state = hover_map['points'][0]['location']
+        fig.data = [trace for trace in fig.data if trace.name != "hoverbar"]
+        highlight_state(fig, hovered_state, 'hoverstate')
 
-        if hover_map:
-            hovered_state = hover_map['points'][0]['location']
-        # Extract the geometry of the hovered state
-        hovered_geometry = None
-        for feature in us_states['features']:
-            if feature['properties']['name'] == hovered_state:
-                hovered_geometry = feature['geometry']
-                break
+    if hover_bar:
+        hovered_state = hover_bar['points'][0]['label']
+        fig.data = [trace for trace in fig.data if trace.name != "hoverstate"]
+        highlight_state(fig, hovered_state, 'hoverbar')
 
-        # If geometry is found, highlight it
-        if hovered_geometry:
-            lats, lons = [], []
-            if hovered_geometry['type'] == 'Polygon':
-                for coords in hovered_geometry['coordinates']:
-                    lats.extend([point[1] for point in coords])
-                    lons.extend([point[0] for point in coords])
-            elif hovered_geometry['type'] == 'MultiPolygon':
-                for polygon in hovered_geometry['coordinates']:
-                    for coords in polygon:
-                        lats.extend([point[1] for point in coords])
-                        lons.extend([point[0] for point in coords])
-
-            fig.add_trace(
-                go.Scattermapbox(
-                    lat=lats,
-                    lon=lons,
-                    mode='lines',
-                    line=dict(
-                        width=1,
-                        color='lightgrey',
-
-                    ),
-                    opacity=0.2,
-                    hoverinfo='skip',
-                    name='Highlight_Hover'
-                )
-            )
-        fig.update_mapboxes(uirevision=True)
-
-    if click_data or click_bar:
-        if click_bar:
-            selected_state = click_bar['points'][0]['label']
-
-        if click_data:
-            selected_state = click_data['points'][0]['location']
+    if click_bar:
+        selected_state = click_bar['points'][0]['label']
         df_state = df[df['state_name'] == selected_state]
+        fig.data = [trace for trace in fig.data if trace.name != "clickstate"]
+        add_points(fig, df_state, selected_state, 'clickbar')
+        highlight_state(fig, selected_state, 'clickbar')
 
-        fig.add_trace(
-            go.Scattermapbox(
-                lat=df_state['Latitude'],
-                lon=df_state['Longitud'],
-                mode='markers',
-                marker=dict(
-                    size=6,
-                    color='darkred',
-                    opacity=0.5
-                ),
-                hoverinfo='skip',
+    if click_data:
+        selected_state = click_data['points'][0]['location']
+        df_state = df[df['state_name'] == selected_state]
+        fig.data = [trace for trace in fig.data if trace.name != "clickbar"]
+        add_points(fig, df_state, selected_state, 'clickstate')
+        highlight_state(fig, selected_state, 'clickstate')
+
+    if 'mapbox.zoom' in relayout:
+        if relayout['mapbox.zoom'] >= 4:
+            add_points(fig, df, None, 'all_points')
+    return fig, bar
+
+
+def add_points(fig, df_state, selected_state, name):
+    fig.add_trace(
+        go.Scattermapbox(
+            lat=df_state['Latitude'],
+            lon=df_state['Longitud'],
+            mode='markers',
+            marker=dict(
+                size=6,
+                color='darkred',
+                opacity=0.5
             ),
-        )
+            hoverinfo='skip',
+            name=name
+        ),
+    )
 
-        selected_geometry = None
-        for feature in us_states['features']:
-            if feature['properties']['name'] == selected_state:
-                selected_geometry = feature['geometry']
-                break
-
-        if selected_geometry and selected_geometry['type'] == 'Polygon':
-            for coords in selected_geometry['coordinates']:
-                fig.add_trace(
-                    go.Scattermapbox(
-                        lon=[point[0] for point in coords],
-                        lat=[point[1] for point in coords],
-                        mode='lines',
-                        line=dict(color='lightgrey', width=1),
-                        hoverinfo='skip',
-                        opacity=0.2
-                    )
-                )
-        elif selected_geometry and selected_geometry['type'] == 'MultiPolygon':
-            for polygon in selected_geometry['coordinates']:
-                for coords in polygon:
-                    fig.add_trace(
-                        go.Scattermapbox(
-                            lon=[point[0] for point in coords],
-                            lat=[point[1] for point in coords],
-                            mode='lines',
-                            line=dict(color='lightgrey', width=1),
-                            hoverinfo='skip',
-                            opacity=0.2
-                        )
-                    )
-
+    if selected_state is not None:
         fig.update_mapboxes(
             zoom=5,
             center={
@@ -239,9 +189,47 @@ def update_map(hover_map, hover_bar, click_bar, click_data):
             },
         )
 
-    return fig, bar
+
+def highlight_state(fig, hovered_state, trace_name):
+    """
+    Adds a highlight trace for a hovered state to the figure.
+    """
+    hovered_geometry = None
+    for feature in us_states['features']:
+        if feature['properties']['name'] == hovered_state:
+            hovered_geometry = feature['geometry']
+            break
+
+    if hovered_geometry and hovered_geometry['type'] == 'Polygon':
+        for coords in hovered_geometry['coordinates']:
+            fig.add_trace(
+                go.Scattermapbox(
+                    lon=[point[0] for point in coords],
+                    lat=[point[1] for point in coords],
+                    mode='lines',
+                    line=dict(color='lightgrey', width=1),
+                    hoverinfo='skip',
+                    opacity=0.6,
+                    name=trace_name,
+                )
+            )
+    elif hovered_geometry and hovered_geometry['type'] == 'MultiPolygon':
+        for polygon in hovered_geometry['coordinates']:
+            for coords in polygon:
+                fig.add_trace(
+                    go.Scattermapbox(
+                        lon=[point[0] for point in coords],
+                        lat=[point[1] for point in coords],
+                        mode='lines',
+                        line=dict(color='lightgrey', width=1),
+                        hoverinfo='skip',
+                        opacity=0.6,
+                        name=trace_name,
+                    )
+                )
+    fig.update_mapboxes(uirevision=True)
+    fig.update_layout(hovermode='closest')
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
